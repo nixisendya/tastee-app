318d641fafc1c0038c7badda87a8881faa1bdf20
nixi.sendya <nixi.sendya@ui.ac.id>
2019-12-11 16:54:03 +0700

    Implemented permission rationale

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/nixisendyaputri/tasteeapp/activities/CookRecipeActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/nixisendyaputri/tasteeapp/activities/CookRecipeActivity.kt
@@ -3,6 +3,7 @@ package id.ac.ui.cs.mobileprogramming.nixisendyaputri.tasteeapp.activities
 import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build.*
@@ -11,6 +12,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProviders
 import com.google.android.youtube.player.YouTubeStandalonePlayer
 import id.ac.ui.cs.mobileprogramming.nixisendyaputri.tasteeapp.R
@@ -82,30 +84,76 @@ class CookRecipeActivity : AppCompatActivity(){
             if (VERSION.SDK_INT >= VERSION_CODES.M){
                 if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
                     PackageManager.PERMISSION_DENIED){
-                    //permission denied
-                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
-                    //show popup to request runtime permission
-                    requestPermissions(permissions, PERMISSION_CODE);
+//                    //permission denied
+//                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
+//                    //show popup to request runtime permission
+//                    requestPermissions(permissions, PERMISSION_CODE);
+
+                    // Permission is not granted
+                    // Should we show an explanation?
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                        // Show an explanation to the user *asynchronously* -- don't block
+                        // this thread waiting for the user's response! After the user
+                        // sees the explanation, try again to request the permission.
+
+                        val builder = AlertDialog.Builder(this)
+                        builder.setMessage("Permission to access the external storage is needed for app to access your gallery.")
+                                .setTitle("Permission required")
+
+                        builder.setPositiveButton("OK"
+                        ) { dialog, id ->
+                            ActivityCompat.requestPermissions(this,
+                                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                                PERMISSION_CODE)
+                        }
+
+                        val dialog = builder.create()
+                        dialog.show()
+
+                        button_add_photo_from_gallery.visibility = View.GONE
+                        button_save_photo_from_gallery.visibility = View.VISIBLE
+                        textview_change_photo.visibility = View.VISIBLE
+
+                    } else {
+                        // No explanation needed, we can request the permission.
+                        ActivityCompat.requestPermissions(this,
+                            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                            PERMISSION_CODE)
+                    }
                 }
                 else{
                     //permission already granted
                     pickImageFromGallery();
+
+                    button_add_photo_from_gallery.visibility = View.GONE
+                    button_save_photo_from_gallery.visibility = View.VISIBLE
+                    textview_change_photo.visibility = View.VISIBLE
                 }
             }
             else{
                 //system OS is < Marshmallow
                 pickImageFromGallery();
+
+                button_add_photo_from_gallery.visibility = View.GONE
+                button_save_photo_from_gallery.visibility = View.VISIBLE
+                textview_change_photo.visibility = View.VISIBLE
             }
 
-            button_add_photo_from_gallery.visibility = View.GONE
-            button_save_photo_from_gallery.visibility = View.VISIBLE
-            textview_change_photo.visibility = View.VISIBLE
+//            button_add_photo_from_gallery.visibility = View.GONE
+//            button_save_photo_from_gallery.visibility = View.VISIBLE
+//            textview_change_photo.visibility = View.VISIBLE
 
         }
 
         button_save_photo_from_gallery.setOnClickListener{
-            saveList()
-            finish()
+            if(photoToUpload==""){
+                Toast.makeText(this, getString(R.string.toast_empty_photo), Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                saveList()
+                finish()
+            }
         }
 
         textview_change_photo.setOnClickListener{
@@ -160,7 +208,7 @@ class CookRecipeActivity : AppCompatActivity(){
                 }
                 else{
                     //permission from popup denied
-                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, getString(R.string.toast_permission_denied), Toast.LENGTH_SHORT).show()
                 }
             }
         }
