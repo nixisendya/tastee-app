4050cccb2e208d529ada793dc473124acbc57989
nixi.sendya <nixi.sendya@ui.ac.id>
2019-12-12 15:32:27 +0700

    Minor fixes

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/nixisendyaputri/tasteeapp/activities/CookRecipeActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/nixisendyaputri/tasteeapp/activities/CookRecipeActivity.kt
@@ -84,10 +84,6 @@ class CookRecipeActivity : AppCompatActivity(){
             if (VERSION.SDK_INT >= VERSION_CODES.M){
                 if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
                     PackageManager.PERMISSION_DENIED){
-//                    //permission denied
-//                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE);
-//                    //show popup to request runtime permission
-//                    requestPermissions(permissions, PERMISSION_CODE);
 
                     // Permission is not granted
                     // Should we show an explanation?
